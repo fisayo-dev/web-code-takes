76ec9cb71f428900badd604e6ba76ea741b79b0d
fisayo-dev <olufisayobadina@gmail.com>
2026-07-20 09:35:24 +0100

    chore: proxy next.js route

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import type { ApiResponse, LoginPayload, OtpPayload, SignupPayload, User } from "./types"
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
